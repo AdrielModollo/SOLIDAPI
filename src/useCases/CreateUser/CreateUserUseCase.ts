fb9a7e49de94c8e113e8985bc6b84455e28786e3
Adriel Modollo <adrielmodollo@outlook.com>
2020-12-15 18:29:07 -0300

    Providers

--- src/useCases/CreateUser/CreateUserUseCase.ts
+++ src/useCases/CreateUser/CreateUserUseCase.ts
@@ -2,11 +2,13 @@
 import { User } from "../../entities/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 import { ICreateUserRequestDTO } from "./CreateUserDTO";
-
+import { IMailProvider } from "../../providers/IMailProvider";
 //Não tem responsabilidade de saber onde será salvo o usuário, apenas verifica se usuário existe ou não e cria caso solicitado
 export class CreateUserUseCase {  
     constructor(
-        private usersRepository: IUsersRepository //recebe metódos, não importa qual.
+        //recebe metódos, não importa qual.
+        private usersRepository: IUsersRepository, 
+        private mailProvider: IMailProvider,
     ){}
 
     //depende de uma abstração de uma interface de uma implementação
@@ -14,12 +16,25 @@ export class CreateUserUseCase {
        const userAlreadyExists = await this.usersRepository.findByEmail(data.email) //verifica existência de usuário
     
 
-    if(userAlreadyExists){
-        throw new Error("User Already exists.");
-    }
+        if(userAlreadyExists){
+            throw new Error("User Already exists.");
+        }
 
-    const user = new User(data);
+        const user = new User(data);
 
-    await this.usersRepository.save(user); //salva nosso usuário
-    }
-}
+        await this.usersRepository.save(user); //salva nosso usuário
+        
+        await this.mailProvider.sendMail({
+            to: {
+              name: data.name,
+              email: data.email,
+            },
+            from: {
+              name: 'Equipe do Meu App',
+              email: 'equipe@meuapp.com',
+            },
+            subject: 'Seja bem-vindo à plataforma',
+            body: '<p>Você já pode fazer login em nossa plataforma.</p>'
+          })
+        }
+      }
